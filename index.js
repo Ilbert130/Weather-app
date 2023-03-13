6c19d9e7699790334d81b8c5d2d09719420a4a50
Ilbert130 <ilbertca27@gmail.com>
2023-03-14 06:13:35 +0800

    Modelo buscqueda creado

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import { inquirerMenu, leerInput, pausa } from "./helpers/inquirer.js";
+import { Busquedas } from "./models/busquedas.js";
 
 
 const main = async() =>{
     
     let opt = '';
+    const buquedas = new Busquedas();
 
     do{
         opt = await inquirerMenu();
         console.log(opt);
+
+        switch(opt){
+            case 1:
+                //Mostrar mensaje
+
+                //Buscar los lugares
+
+                //Seleccionar el lugar
+
+                //Clima
+
+                //Mostar datos del clima
+                break
+        }
 
         if(opt !== 0) await pausa();
 
---
+++ models/busquedas.js
@@ -0,0 +1,23 @@
+
+
+class Busquedas{
+
+    historial = ['Madrid', 'San Jose'];
+
+    constructor(){
+        //TODO: leer DB si existe
+    }
+
+    async ciudad(lugar=''){
+        
+        //peticion http
+        console.log(lugar);
+
+        return []; //retornar los lugares 
+    }
+}
+
+
+export {
+    Busquedas
+}
